964bc5fe801f4dca32770b933207eb38b4c925b1
Bruce Walker <8148028+bdwalker1@users.noreply.github.com>
2025-03-28 21:27:58 -0700

    Tweaks.

--- demo_page/app/src/appvars.py
+++ demo_page/app/src/appvars.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from pydantic import BaseModel, ConfigDict
 from uuid import UUID, uuid4
 
@@ -5,6 +7,8 @@ class SessionData(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
     session_id: str = None
     api_session_id: str = None
+    created_at: datetime = None
+    last_access: datetime = None
 
 def init():
     global api_url, sessions_db
@@ -13,26 +17,32 @@ def init():
 
 def get_session_data(session_id):
     global sessions_db
+    clear_old_sessions()
     is_new_session = False
     print(F"session_id: {session_id}")
     if (session_id == None) or (not is_uuid(session_id)):
         is_new_session = True
         session_id = uuid4()
         session_data = SessionData()
+        session_data.created_at = datetime.now()
+        session_data.last_access = datetime.now()
         session_data.session_id = str(session_id)
-        sessions_db[session_id] = session_data
     else:
         session_data = sessions_db.get(session_id)
         if session_data == None:
             is_new_session = True
             session_data = SessionData()
+            session_data.created_at = datetime.now()
             session_data.session_id = str(session_id)
-            sessions_db[session_id] = session_data
+    session_data.last_access = datetime.now()
+    sessions_db[session_id] = session_data
     return session_id, session_data, is_new_session
 
 def update_session_data(session_id: UUID, session_data: SessionData):
     global sessions_db
+    clear_old_sessions()
     session_data.session_id = str(session_id)
+    session_data.last_access = datetime.now()
     sessions_db[session_id] = session_data
     return
 
@@ -46,3 +56,9 @@ def is_uuid(uuid_to_test, version=4):
     except ValueError:
         return False
     return True
+
+def clear_old_sessions():
+    for session_id, session_data in sessions_db.items():
+        if session_data.last_access < (datetime.now() - relativedelta(days=1)):
+            del sessions_db[session_id]
+    return
